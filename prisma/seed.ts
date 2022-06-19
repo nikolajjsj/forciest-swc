90b4e5911107bb6873ee5fc7e33a08a2a7b8c577
nikolajjsj <10490273+nikolajjsj@users.noreply.github.com>
2022-06-19 17:06:29 +0200

    feat: remove old prisma stuff

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,16 @@
-/**
- * Adds seed data to your db
- *
- * @link https://www.prisma.io/docs/guides/database/seed-database
- */
 import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 async function main() {
-  const firstPostId = '5c03994c-fc16-47e0-bd02-d218a370a078';
-  await prisma.post.upsert({
+  const firstCharId = 1;
+  await prisma.character.upsert({
     where: {
-      id: firstPostId,
+      charId: firstCharId,
     },
     create: {
-      id: firstPostId,
-      title: 'First Post',
-      text: 'This is an example post generated from `prisma/seed.ts`',
+      charId: firstCharId,
+      likes: 1,
     },
     update: {},
   });
---
+++ src/components/stitches/character.ts
@@ -0,0 +1,6 @@
+import { styled } from '../../../stitches.config';
+
+export const Character = styled('div', {
+  display: 'flex',
+  flexDirection: 'column',
+});
